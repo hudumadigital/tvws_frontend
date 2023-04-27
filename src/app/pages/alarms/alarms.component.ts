378f20ecafa09ce2bc6c1dc1b122a2567907edb2
Muneersahel <saidmunir73@gmail.com>
2023-04-27 19:27:45 +0300

    add alart table

--- src/app/pages/alarms/alarms.component.ts
+++ src/app/pages/alarms/alarms.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
 import { EzvizCameraService } from 'src/app/services/ezviz-camera.service';
 
 @Component({
   selector: 'app-alarms',
   standalone: true,
   imports: [CommonModule],
-  template: ` <p>Event Notification works!</p> `,
+  templateUrl: './alarms.component.html',
   styleUrls: ['./alarms.component.scss'],
 })
-export class AlarmsComponent implements OnInit{
+export class AlarmsComponent implements OnInit {
   private cameraService = inject(EzvizCameraService);
   alarms: Array<string> = [];
   constructor() {}
