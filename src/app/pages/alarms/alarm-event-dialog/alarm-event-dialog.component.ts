d8f47be4d7bb61097b664674e337c3e2c9473393
corama <coramayunus@gmail.com>
2023-06-28 01:01:26 +0300

    Merged the codes

--- src/app/pages/alarms/alarm-event-dialog/alarm-event-dialog.component.ts
+++ src/app/pages/alarms/alarm-event-dialog/alarm-event-dialog.component.ts
@@ -1,7 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 import { Component, DestroyRef, inject, signal } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import {
